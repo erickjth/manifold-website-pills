dfd7553aaffb6a3da986597c8dbd2ffd6348a356
Erick Torres <erickt@digintent.com>
2022-10-12 11:36:23 -0500

    Updates

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		// sourcemap: false,
+		// minify: false,
+		rollupOptions: {
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+				manualChunks: undefined,
+				entryFileNames: 'assets/app-[name].js',
+			},
+		},
+	},
+});
